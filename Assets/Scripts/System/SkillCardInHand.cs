a593041b784ce741b39c10d0e861adfad30cc5a3
seungyunLee <esyun602@gmail.com>
2025-07-28 18:07:14 +0900

    글로벌 스킬카드 사용 분리

--- Assets/Scripts/System/SkillCardInHand.cs
+++ Assets/Scripts/System/SkillCardInHand.cs
@@ -14,7 +14,7 @@ public class SkillCardInHand : BattleCardObjectInHand
 	private SkillCardBattleStat battleStat;
 	protected override ICard TargetCard => targetCard;
 	public override IStat Stat => battleStat;
-	
+
 	public static SkillCardInHand Instantiate(SkillCard targetSkillCard, SkillCardBattleStat skillCardStat)
 	{
 		var cardObject = GameObject.Instantiate(Resources.Load(cardPrefabPath)).AddComponent<SkillCardInHand>();
@@ -34,28 +34,31 @@ public class SkillCardInHand : BattleCardObjectInHand
 		{
 			return false;
 		}
-		
+
 		var bo = map.GetBattleObjectOfTile(tile);
 		return bo != null
-		       && cardObjectStateMachine.CurrentState is SkillCardSelectedInHandState;
+		       && cardObjectStateMachine.CurrentState is TargetingSkillCardSelectedInHandState;
 	}
-	
+
 	protected override void OnPointerClickImpl(PointerEventData eventData)
 	{
 		if ((blockInput & InputBlockFlag.Select) != InputBlockFlag.None || !CanSelect()) return;
-		if (cardObjectStateMachine.CurrentState is CardObjectNormalInHandState { IsHovered: true } &&
-		    eventData.button == PointerEventData.InputButton.Left)
+		if (cardObjectStateMachine.CurrentState is not CardObjectNormalInHandState { IsHovered: true } ||
+		    eventData.button != PointerEventData.InputButton.Left) return;
+
+		NoticeSystem.Instance.PublishSync(new SkillHandCardSelectNotice(this));
+
+		if (IsTargeting)
 		{
-			NoticeSystem.Instance.PublishSync(new SkillHandCardSelectNotice(this));
-			ChangeState(new SkillCardSelectedInHandState(this));
+			ChangeState(new TargetingSkillCardSelectedInHandState(this));
+		}
+		else
+		{
+			ChangeState(new GlobalSkillCardSelectedInHandState(this));
 		}
 	}
-	
-	
-	/// <summary>
-	/// 마우스 포인터를 따라가는 상태
-	/// </summary>
-	private class SkillCardSelectedInHandState : IState, IUpdatable
+
+	private class TargetingSkillCardSelectedInHandState : IState, IUpdatable
 	{
 		public bool IsMoving => targetPos != owner.transform.position;
 		private SkillCardInHand owner;
@@ -66,7 +69,7 @@ public class SkillCardInHand : BattleCardObjectInHand
 		private float timePassed = 0f;
 		private ITile currentTile;
 
-		public SkillCardSelectedInHandState(SkillCardInHand owner)
+		public TargetingSkillCardSelectedInHandState(SkillCardInHand owner)
 		{
 			this.owner = owner;
 		}
@@ -96,13 +99,13 @@ public class SkillCardInHand : BattleCardObjectInHand
 				//todo: 이런 상황이 발생하면 안되는데
 				throw new Exception();
 			}
-			
+
 			//todo: 타일이 필요 없는 카드
 			if (owner.CanUse(currentTile))
 			{
 				//todo: 사용함수를 분리?
 				Game.Instance.GetGameMode<BattleStageGameMode>().DeckSystem.Energy -= owner.battleStat.CostValue;
-				owner.ChangeState(new CardObjectUsedInHandState(owner, currentTile));
+				owner.ChangeState(new TargetingCardObjectUsedInHandState(owner, currentTile));
 			}
 		}
 
@@ -129,8 +132,9 @@ public class SkillCardInHand : BattleCardObjectInHand
 
 			currentTile = Game.Instance.GetGameMode<StageGameMode>().GetCurrentStage().Map.GetTileAt(mousePos);
 			targetPos = owner.CanUse(currentTile) ? currentTile.GetPosition().GetX0z(Constant.SelectYPos) : mousePos;
-			NoticeSystem.Instance.Publish(new SkillHandCardTargetingUpdateNotice(Camera.main.WorldToScreenPoint(targetPos)));
-			
+			NoticeSystem.Instance.Publish(
+				new SkillHandCardTargetingUpdateNotice(Camera.main.WorldToScreenPoint(targetPos)));
+
 			if (Vector3.Distance(targetPos, owner.transform.position) < 0.01f)
 			{
 				timePassed = 0f;
@@ -149,27 +153,112 @@ public class SkillCardInHand : BattleCardObjectInHand
 			                                Camera.main.transform.localRotation;*/
 		}
 	}
-	
-	//todo: run action
-	private class CardObjectUsedInHandState : IState, IUpdatable
+
+	private class GlobalSkillCardSelectedInHandState : IState, IUpdatable
+	{
+		public bool IsMoving => targetPos != owner.transform.position;
+		private SkillCardInHand owner;
+		private Vector3 targetPos;
+		private Quaternion targetRotation;
+		private const float followSpeed = 400f;
+		private AnimationCurve followAnimationCurve;
+		private float timePassed = 0f;
+
+		public GlobalSkillCardSelectedInHandState(SkillCardInHand owner)
+		{
+			this.owner = owner;
+		}
+
+		public void Enter(IState prevState)
+		{
+			InputManager.Instance.InputActions.Player.UseHandCard.Enable();
+			InputManager.Instance.InputActions.Player.CancelHandCard.Enable();
+			InputManager.Instance.InputActions.Player.UseHandCard.performed += OnTryUseHandCard;
+			InputManager.Instance.InputActions.Player.CancelHandCard.performed += OnCancelHandCard;
+			owner.transform.up = Camera.main.transform.up;
+			//todo: fix
+			owner.transform.localScale = Vector3.one * 1.8f;
+			owner.transform.position = owner.hoverTargetPos;
+			followAnimationCurve = GameDataSystem.Instance.GetGameData<Constant>().CardFollowingSpeedCurve;
+
+			var mouseScreenPos = Input.mousePosition;
+			mouseScreenPos.z = 10f;
+			targetPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+			owner.transform.position = targetPos.GetX0z(Constant.SelectYPos);
+		}
+
+		private void OnTryUseHandCard(InputAction.CallbackContext obj)
+		{
+			if (!owner.CanSelect())
+			{
+				//todo: 이런 상황이 발생하면 안되는데
+				throw new Exception();
+			}
+
+			Game.Instance.GetGameMode<BattleStageGameMode>().DeckSystem.Energy -= owner.battleStat.CostValue;
+			owner.ChangeState(new GlobalCardObjectUsedInHandState(owner));
+		}
+
+		public void Exit(IState nextState)
+		{
+			InputManager.Instance.InputActions.Player.UseHandCard.Disable();
+			InputManager.Instance.InputActions.Player.CancelHandCard.Disable();
+			InputManager.Instance.InputActions.Player.UseHandCard.performed -= OnTryUseHandCard;
+			InputManager.Instance.InputActions.Player.CancelHandCard.performed -= OnCancelHandCard;
+		}
+
+		private void OnCancelHandCard(InputAction.CallbackContext obj)
+		{
+			NoticeSystem.Instance.PublishSync(new SkillHandCardSelectCancelNotice(owner));
+			owner.ChangeState(new CardObjectNormalInHandState(owner));
+		}
+
+		public void UpdateFrame(float dt)
+		{
+			//todo: optimize and fix - new input mouse pos not working
+			var mouseScreenPos = Input.mousePosition;
+			mouseScreenPos.z = 10f;
+			var mousePos = Camera.main.ScreenToWorldPoint(mouseScreenPos).GetX0z(Constant.SelectYPos);
+
+			targetPos = mousePos;
+
+			if (Vector3.Distance(targetPos, owner.transform.position) < 0.01f)
+			{
+				timePassed = 0f;
+			}
+
+			timePassed += dt;
+
+			var realSpeed = followAnimationCurve.Evaluate(timePassed) * followSpeed;
+			var totalTime = Vector3.Distance(targetPos, owner.transform.position) / realSpeed;
+			owner.transform.position = Vector3.Lerp(owner.transform.position, targetPos, dt / totalTime);
+			owner.transform.localRotation = Quaternion.AngleAxis(Mathf.Clamp(
+					                                Vector3.Distance(targetPos, owner.transform.position) * 50f *
+					                                (targetPos.x > owner.transform.position.x ? -1f : 1f), -45f, 45f),
+				                                Vector3.Cross(Camera.main.transform.forward,
+					                                (targetPos - owner.transform.position).normalized)) *
+			                                Camera.main.transform.localRotation;
+		}
+	}
+
+	private class TargetingCardObjectUsedInHandState : IState, IUpdatable
 	{
 		private SkillCardInHand owner;
 		private IBattleObject targetObject;
 		private float timePassed;
 		private Action currentUpdateAction;
-		
-		public CardObjectUsedInHandState(SkillCardInHand owner, ITile targetTile)
+
+		public TargetingCardObjectUsedInHandState(SkillCardInHand owner, ITile targetTile)
 		{
 			var map = Game.Instance.GetGameMode<StageGameMode>().GetCurrentStage().Map;
 			this.owner = owner;
-			this.targetObject = map.GetBattleObjectOfTile(targetTile);;
-			
+			this.targetObject = map.GetBattleObjectOfTile(targetTile);
 		}
 
 		public void Enter(IState prevState)
 		{
 			NoticeSystem.Instance.Publish(new SkillHandCardStartUseNotice(owner));
-			currentUpdateAction = UpdatePreAttack;
+			currentUpdateAction = UpdatePreAction;
 			timePassed = 0f;
 		}
 
@@ -177,8 +266,8 @@ public class SkillCardInHand : BattleCardObjectInHand
 		{
 			NoticeSystem.Instance.Publish(new SkillHandCardEndUseNotice(owner));
 		}
-		
-		private void UpdatePreAttack()
+
+		private void UpdatePreAction()
 		{
 			timePassed += Time.deltaTime;
 			if (timePassed > 0f)
@@ -186,22 +275,88 @@ public class SkillCardInHand : BattleCardObjectInHand
 				timePassed = 0f;
 				owner.targetCard.Action.Trigger(new TargetingActionTriggerInfo()
 				{
-					Target = targetObject 
+					Target = targetObject
 				});
-				currentUpdateAction = UpdateAttack;
+				currentUpdateAction = UpdateAction;
 			}
 		}
 
-		private void UpdateAttack()
+		private void UpdateAction()
 		{
 			owner.targetCard.Action.UpdatableRoutine.UpdateFrame(Time.deltaTime, out var routineDone);
 			if (routineDone)
 			{
-				currentUpdateAction = UpdateEndAttack;
+				currentUpdateAction = UpdateEndAction;
 			}
 		}
 
-		private void UpdateEndAttack()
+		private void UpdateEndAction()
+		{
+			timePassed += Time.deltaTime;
+			if (timePassed > 0f)
+			{
+				timePassed = 0f;
+				currentUpdateAction = null;
+			}
+		}
+
+
+		public void UpdateFrame(float dt)
+		{
+			currentUpdateAction?.Invoke();
+			if (currentUpdateAction == null)
+			{
+				Game.Instance.GetGameMode<BattleStageGameMode>().DeckSystem.DropCard(owner);
+				owner.Deactivate();
+			}
+		}
+	}
+	
+	private class GlobalCardObjectUsedInHandState : IState, IUpdatable
+	{
+		private SkillCardInHand owner;
+		private IBattleObject targetObject;
+		private float timePassed;
+		private Action currentUpdateAction;
+
+		public GlobalCardObjectUsedInHandState(SkillCardInHand owner)
+		{
+			this.owner = owner;
+		}
+
+		public void Enter(IState prevState)
+		{
+			NoticeSystem.Instance.Publish(new SkillHandCardStartUseNotice(owner));
+			currentUpdateAction = UpdatePreAction;
+			timePassed = 0f;
+		}
+
+		public void Exit(IState nextState)
+		{
+			NoticeSystem.Instance.Publish(new SkillHandCardEndUseNotice(owner));
+		}
+
+		private void UpdatePreAction()
+		{
+			timePassed += Time.deltaTime;
+			if (timePassed > 0f)
+			{
+				timePassed = 0f;
+				owner.targetCard.Action.Trigger();
+				currentUpdateAction = UpdateAction;
+			}
+		}
+
+		private void UpdateAction()
+		{
+			owner.targetCard.Action.UpdatableRoutine.UpdateFrame(Time.deltaTime, out var routineDone);
+			if (routineDone)
+			{
+				currentUpdateAction = UpdateEndAction;
+			}
+		}
+
+		private void UpdateEndAction()
 		{
 			timePassed += Time.deltaTime;
 			if (timePassed > 0f)
